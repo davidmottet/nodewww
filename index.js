e0a098d3f9c98aa4fb93ec8941d08f0b78580684
David MOTTET <david.mottet@macbook-pro-de-david.home>
2018-01-26 00:41:57 +0100

    absolute path of www folder

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 'use strict';
 
+const { resolve } = require('path');
 const nodewww = require('./lib/nodewww');
+
+// exact location of the file "www"
+const absoluteWwwPath = resolve('./www');
+nodewww.exposeWWW(absoluteWwwPath);
